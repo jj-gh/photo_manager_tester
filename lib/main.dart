eb58d265d130c2e6deb1a4c2c90926eb4926a636
jj-gh <jj-gh@github.com>
2020-07-16 01:27:13 +0800

    thumbnailDataWithSize() returns full size on android 7.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var result = await PhotoManager.requestPermission();
+  if (result)
+    runApp(MyApp());
+  else
+    PhotoManager.openSetting();
 }
 
 class MyApp extends StatefulWidget {
@@ -11,24 +17,43 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<int> _thumb;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
         body: Container(
             alignment: Alignment.center,
-            child: RaisedButton(
-              child: Text('Click Me'),
-              onPressed: () async {
-                List<AssetPathEntity> albums =
-                    await PhotoManager.getAssetPathList();
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _thumb != null ? Image.memory(_thumb) : Container(),
+                RaisedButton(
+                  child: Text('Load Thumbnail'),
+                  onPressed: () async {
+                    List<AssetPathEntity> albums =
+                        await PhotoManager.getAssetPathList();
 
-                // this will crash the app
-                List<AssetEntity> medias = await albums[0]
-                    .getAssetListRange(start: 0, end: albums[0].assetCount);
-              },
+                    List<AssetEntity> medias = await albums[0]
+                        .getAssetListRange(start: 0, end: albums[0].assetCount);
+
+                    // returns full size on first load
+                    _thumb =
+                        await medias[1].thumbDataWithSize(ThumbOption(50, 50));
+
+                    setState(() {});
+                  },
+                ),
+              ],
             )),
       ),
     );
   }
+}
+
+class ThumbOption extends LoadOption {
+  ThumbOption(int width, int height) : super(width, height, quality: 75);
 }
